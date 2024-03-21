dbf49f4af142cb97e011c2a4fab5752e3acdf5d5
est3banst <estebeeen@outlook.com>
2024-03-21 02:08:58 -0300

    added pics

--- src/componentes/data/picts.js
+++ src/componentes/data/picts.js
@@ -1,20 +1,25 @@
+import Hero from '/imgs/installed.jpeg';
 import Hero1 from '/imgs/hugohero.png';
 import Hero2 from '/imgs/hugohero2.jpg';
 import Hero3 from '/imgs/hugoheronow.jpg';
-import Hero4 from '/imgs/reforma.jpg';
+import Hero4 from '/imgs/tablerosteel.jpeg';
 
 
 export const picsSection = [
+    
     {id:1,
-    imgSrc: Hero1
+    imgSrc: Hero
     },
     {id:2,
-    imgSrc: Hero2
+    imgSrc: Hero1
     },
     {id:3,
+    imgSrc: Hero2
+    },
+    {id:4,
     imgSrc: Hero3
     },
-    {id: 4,
+    {id: 5,
     imgSrc: Hero4
     },
 ]
