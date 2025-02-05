72995d84877b5b200a1aeadc99715b96244e9aaf
est3banst <estebeeen@outlook.com>
2025-02-05 18:10:31 -0300

    d st

--- src/componentes/Hero/HeroSecond.jsx
+++ src/componentes/Hero/HeroSecond.jsx
@@ -16,32 +16,32 @@ const HeroSecond = () => {
     
   return (
     <>
-     <section className='h-auto w-11/12 m-auto flex flex-col md:flex-row my-4'>
-            <div className='h-4/5 cust-border my-3'>
-                <h2 className='text-neutral-900 font-bold text-xl md:text-2xl lg:text-3xl'>
+     <section className='cust-border h-auto w-full m-auto flex flex-col md:flex-row p-6'>
+            <div className='h-auto my-3 cust-bg-section'>
+                <h2 className='text-slate-100 md:text-slate-50 font-bold text-xl md:text-2xl lg:text-3xl'>
                     Soluciones eficientes para tu hogar, empresa o negocio
                 </h2>
                 <hr />
-                <section className='py-4 section-for-pfo'>
-                    <p className='text-neutral-900 py-2 text-base md:text-xl lg:text-2xl'>Red fibra óptica
+                <section className='py-4 section-for-pfo flex flex-col gap-2'>
+                    <p className='text-slate-100 font-semibold underline md:text-slate-50 py-2 text-base md:text-xl lg:text-2xl'>Red fibra óptica
                     </p>
-                    <p className='text-neutral-900 py-2 text-base md:text-xl lg:text-2xl'>Instalaciones Eléctricas
+                    <p className='text-slate-100 font-semibold underline md:text-slate-50 py-2 text-base md:text-xl lg:text-2xl'>Instalaciones Eléctricas
                     </p>
-                    <p className='text-neutral-900 py-2 text-base md:text-xl lg:text-2xl'>Reparaciones Eléctricas
+                    <p className='text-slate-100 font-semibold underline md:text-slate-50 py-2 text-base md:text-xl lg:text-2xl'>Reparaciones Eléctricas
                     </p>
-                    <p className='text-neutral-900 py-2 text-base md:text-xl lg:text-2xl'>Instalación de Cámaras de Seguridad
+                    <p className='text-slate-100 font-semibold underline md:text-slate-50 py-2 text-base md:text-xl lg:text-2xl'>Instalación de Cámaras de Seguridad
                     </p>
-                    <p className='text-neutral-900 py-2 text-base md:text-xl lg:text-2xl'>Instalación de Redes y Wi-fi
+                    <p className='text-slate-100 font-semibold underline md:text-slate-50 py-2 text-base md:text-xl lg:text-2xl'>Instalación de Redes y Wi-fi
                     </p>
-                    <p className='text-neutral-900 py-2 text-base md:text-xl lg:text-2xl'>Porteros Eléctricos
+                    <p className='text-slate-100 font-semibold underline md:text-slate-50 py-2 text-base md:text-xl lg:text-2xl'>Porteros Eléctricos
                     </p>
-                    <p className='text-neutral-900 py-2 text-base md:text-xl lg:text-2xl'>Instalación de Paneles Solares
+                    <p className='text-slate-100 font-semibold underline md:text-slate-50 py-2 text-base md:text-xl lg:text-2xl'>Instalación de Paneles Solares
                     </p>
-                    <p className='text-neutral-900 py-2 text-base md:text-xl lg:text-2xl'>Iluminaria e Iluminación Led
+                    <p className='text-slate-100 font-semibold underline md:text-slate-50 py-2 text-base md:text-xl lg:text-2xl'>Iluminaria e Iluminación Led
                     </p>
                 </section>
             </div>
-            <div className='cust-border h-auto w-full flex md:gap-2 flex-col '>
+            <div className='h-auto w-full flex md:gap-2 flex-col '>
             <div ref={imgRef} 
             style={{
                 backgroundImage: 'url(/imgs/panelbox.webp)',
@@ -49,7 +49,7 @@ const HeroSecond = () => {
                 backgroundPosition: '100% 50%',
                 backgroundRepeat: 'no-repeat'
             }}
-            className={`cust-circle py-4 translate-x-[15%] md:translate-x-[20%] transition-all duration-500 ease-in-out w-full min-h-80 h-[320px] ${isHeroVis ? 'reveal-imgs' : ''}`}>
+            className={`cust-circle py-4 translate-x-[15%] transition-all duration-500 ease-in-out w-full min-h-80 h-[320px] ${isHeroVis ? 'reveal-imgs' : ''}`}>
                
                 </div>
             <div ref={img2Ref} 
@@ -59,7 +59,7 @@ const HeroSecond = () => {
                 backgroundPosition: '50% 50%',
                 backgroundRepeat: 'no-repeat'
             }}
-            className={`cust-circle py-2 w-[100%] translate-x-[-15%] transition-all duration-500 ease-in-out md:translate-x-[-20%] min-h-82 h-[340px] ${isAllHeroVis ? 'reveal-img2' : ''}`}>
+            className={`cust-circle py-2 translate-x-[-15%] transition-all duration-500 ease-in-out min-h-82 h-[340px] ${isAllHeroVis ? 'reveal-img2' : ''}`}>
               
                 </div>
                 <hr />
